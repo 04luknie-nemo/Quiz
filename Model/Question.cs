048f805449223e45f84219950e756bcd8e47a011
Lukas <04luknie@skola.boras.se>
2026-02-20 11:24:35 +0100

    Check so quiz can be returned

--- Model/Question.cs
+++ Model/Question.cs
@@ -6,7 +6,7 @@ public class Question
     public int Id { get; set; }
     public string Text { get; set; } = "";
     public List<string> Answers { get; set; }
-    public int CurrectAnswer { get; set; }
+    public int CorrectAnswer { get; set; }
     public List<ParticipantAnswer> participantAnswers { get; set; }
     public List<int> VoteCount { get; set; }
 }
--- QuizManager.cs
+++ QuizManager.cs
@@ -1,8 +1,42 @@
+using Model.ParticipantAnswer;
+using Model.Question;
+using Model.Quiz;
+
 public class QuizManager
 {
-    public void GetQuiz()
+    Dictionary<string, Quizz> quizzes;
+    Quizz quizz = new Quizz()
     {
-
+        Id = 1,
+        Title = "Enkät v8",
+        Questions = new List<Question>
+        {
+            new Question
+            {
+                Id = 1,
+                Text = "Vilket djur är känt för att vara världens snabbaste på land?",
+                Answers = new List<string>
+                {
+                    "Lejon",
+                    "Gepard",
+                    "Antilop",
+                    "Struts"
+                },
+                CorrectAnswer = 1, // Index 1 = "Gepard"
+                participantAnswers = new List<ParticipantAnswer>
+                {
+                    new ParticipantAnswer { ParticipantId = "1", AnswerIndex = 1, TimeStamp = DateTime.Now },
+                    new ParticipantAnswer { ParticipantId = "2", AnswerIndex = 3, TimeStamp = DateTime.Now },
+                    new ParticipantAnswer { ParticipantId = "3", AnswerIndex = 1, TimeStamp = DateTime.Now }
+                },
+                VoteCount = new List<int> { 0, 2, 0, 1 }
+            },
+        },
+        CurrentQuestionIndex = 0
+    };
+    public Quizz GetQuiz()
+    {
+        return quizz;
     }
     public void SubmitAnswer()
     {
