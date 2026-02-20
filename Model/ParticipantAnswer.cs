d5c603d5ec5c9af8a614fe67f1b6969ced7e33f9
Lukas <04luknie@skola.boras.se>
2026-02-20 20:56:29 +0100

    Small updates to modules

--- Model/ParticipantAnswer.cs
+++ Model/ParticipantAnswer.cs
@@ -2,8 +2,8 @@ namespace Model.ParticipantAnswer;
 
 public class ParticipantAnswer
 {
-    public string ParticipantId { get; set; }
-    public string QuestionId { get; set; }
+    public string ParticipantId { get; set; } = "";
+    public string QuestionId { get; set; } = "";
     public int AnswerIndex { get; set; }
     public DateTime TimeStamp { get; set; }
 }
